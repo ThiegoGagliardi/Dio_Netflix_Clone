1eb8acc24ba68869d8450d2911bb3cfdfeaa0378
KELY MELO DE OLIVEIRA <thiego.gagliardi@gmail.com>
2021-03-23 06:51:22 -0300

    Entrega do projeto Netflix clone

---
+++ snydercutflix/js/main.js
@@ -0,0 +1,9 @@
+
+function info(){
+  window.open("https://www.warnerbros.com.br/filmes/snyder-cut");
+}  
+
+function assistir(){
+    window.open("https://play.google.com/store/movies/details/Zack_Snyder_s_Justice_League?id=J6_pNkrPm20.P");
+ }  
+  
